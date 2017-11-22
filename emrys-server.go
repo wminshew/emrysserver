ab977f84b3747f0c3d6af192aff0d682bc76bcf3
wminshew <wminshew@gmail.com>
2017-11-22 09:20:43 -0800

    added user upload folders & data-dir download with unzipping

--- emrys-server.go
+++ emrys-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/subtle"
 	"fmt"
+	"github.com/mholt/archiver"
 	"io"
 	"log"
 	"net/http"
@@ -17,43 +18,75 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func upload(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		// parse multipart Form request; limit memory usage
+		// (residual should end up temporarily on disk)
 		maxMemory := int64(1) << 31
 		err := r.ParseMultipartForm(maxMemory)
 		if err != nil {
 			log.Printf("Error parsing request: %v\n", err)
 		}
 
-		trainTempFile, trainHandler, err := r.FormFile("Train")
-		if err != nil {
-			log.Printf("Error opening form file: %v\n", err)
-		}
-		defer trainTempFile.Close()
-
+		// if doesn't exist yet, create user directory for uploads
 		username, _, _ := r.BasicAuth()
-		trainDir := "./user-upload/" + username + "/"
+		// TODO: add extra director layer for job number; return job number to client
+		userDir := "./user-upload/" + username + "/"
 		// TODO: THIS FEELS DANGEROUS; IS THERE A SAFER WAY?
 		// error behavior without adjusting umask:
 		// directory without execution / writing bits cannot be written to
 		oldUmask := syscall.Umask(022)
-		log.Printf("Old Umask: %v\n", oldUmask)
-		if err = os.MkdirAll(trainDir, 0777); err != nil {
-			log.Printf("Error creating directory %s: %v\n", trainDir, err)
+		if err = os.MkdirAll(userDir, 0777); err != nil {
+			log.Printf("Error creating user directory %s: %v\n", userDir, err)
 		}
 		_ = syscall.Umask(oldUmask)
-		trainPath := trainDir + trainHandler.Filename
-		f, err := os.OpenFile(trainPath, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			log.Printf("Error opening new file for saving: %v\n", err)
-		}
-		defer f.Close()
 
-		n, err := io.Copy(f, trainTempFile)
+		// open reader on Train file
+		trainTempFile, trainHandler, err := r.FormFile("Train")
 		if err != nil {
-			log.Printf("Error copying file from POST /job/upload: %v\n", err)
+			log.Printf("Error reading train form file: %v\n", err)
 		}
+		defer trainTempFile.Close()
+
+		trainPath := userDir + trainHandler.Filename
+		// TODO: would have to chmod this file later to execute; may need to update
+		// file permissions here for ease later
+		trainFile, err := os.OpenFile(trainPath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			log.Printf("Error opening train file: %v\n", err)
+		}
+		defer trainFile.Close()
+
+		n_train, err := io.Copy(trainFile, trainTempFile)
+		if err != nil {
+			log.Printf("Error copying train file to disk: %v\n", err)
+		}
+
+		dataTempFile, dataHandler, err := r.FormFile("DataDir")
+		if err != nil {
+			log.Printf("Error reading data form file: %v\n", err)
+		}
+		defer dataTempFile.Close()
+
+		dataPath := userDir + dataHandler.Filename
+		dataFile, err := os.OpenFile(dataPath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			log.Printf("Error opening data file: %v\n", err)
+		}
+		defer dataFile.Close()
+
+		n_data, err := io.Copy(dataFile, dataTempFile)
+		if err != nil {
+			log.Printf("Error copying data file to disk: %v\n", err)
+		}
+
+		// untar/gzip the file
+		err = archiver.TarGz.Open(dataPath, userDir)
+		if err != nil {
+			log.Printf("Error unzipping data dir: %v\n", err)
+		}
+		defer os.Remove(dataPath)
 
 		// send response to client
-		io.WriteString(w, fmt.Sprintf("%d bytes recieved and saved.\n", n))
+		io.WriteString(w, fmt.Sprintf("%d bytes recieved and saved.\n", n_train+n_data))
 	} else {
 		log.Printf("Upload received non-POST method.\n")
 		io.WriteString(w, "Upload only receives POSTs.\n")
