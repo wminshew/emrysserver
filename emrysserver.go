64a59f54bb7b6dfcd85656bbebbc8a34f4ea9d57
wminshew <wminshew@gmail.com>
2018-04-02 23:35:40 -0700

    moved main server to HTTPS and added HTTP redirect

--- emrysserver.go
+++ emrysserver.go
@@ -16,13 +16,26 @@ func main() {
 
 	mux.HandleFunc("/job/upload", handlers.JobUpload)
 
-	server.Addr = ":8080"
+	server.Addr = ":4430"
 	// server.Handler = handlers.Log(handlers.AuthUser(mux))
 	server.Handler = handlers.Log(mux)
 
 	log.Printf("Initializing database...\n")
 	db.Init()
 
+	const httpPort = ":8080"
+	log.Printf("Starting http re-direct on port %s...\n", httpPort)
+	go func() {
+		log.Fatal(http.ListenAndServe(httpPort, http.HandlerFunc(redirect)))
+	}()
+
 	log.Printf("Listening on port %s...\n", server.Addr)
-	log.Fatal(server.ListenAndServe())
+	log.Fatal(server.ListenAndServeTLS("server.crt", "server.key"))
+}
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	newURL := *r.URL
+	newURL.Scheme = "https"
+	log.Printf("Redirect to: %s", newURL.String())
+	http.Redirect(w, r, newURL.String(), http.StatusTemporaryRedirect)
 }
