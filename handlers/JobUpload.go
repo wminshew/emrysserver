4b674291f03c82af6fab2ec64902cfdc1b563b66
wminshew <wminshew@gmail.com>
2018-04-11 13:18:05 -0700

    Running docker execution; added filepath usage over +, and fixed error throwing to avoid Fatal which kills server

--- handlers/JobUpload.go
+++ handlers/JobUpload.go
@@ -19,7 +19,8 @@ func JobUpload(w http.ResponseWriter, r *http.Request) {
 		maxMemory := int64(1) << 31
 		err := r.ParseMultipartForm(maxMemory)
 		if err != nil {
-			log.Fatalf("Error parsing request: %v\n", err)
+			log.Printf("Error parsing request: %v\n", err)
+			return
 		}
 
 		// TODO: add uuid or some other unique identifier for users [emails can't be used in paths safely]
@@ -27,135 +28,165 @@ func JobUpload(w http.ResponseWriter, r *http.Request) {
 
 		// TODO: re-factor job processing; take out file saving, add relevant paths to r.context
 		// TODO: add extra directory layer for project/job number (git vcs?); return job number to client
-		userDir := "./user-upload/" + username + "/"
+		userDir := filepath.Join("user-upload", username)
 		if err = os.MkdirAll(userDir, 0755); err != nil {
-			log.Fatalf("Error creating user directory %s: %v\n", userDir, err)
+			log.Printf("Error creating user directory %s: %v\n", userDir, err)
+			return
 		}
 
 		requirementsTempFile, requirementsHandler, err := r.FormFile("requirements")
 		if err != nil {
-			log.Fatalf("Error reading requirements form file: %v\n", err)
+			log.Printf("Error reading requirements form file: %v\n", err)
+			return
 		}
 		defer requirementsTempFile.Close()
-		requirementsPath := userDir + filepath.Base(requirementsHandler.Filename)
+		requirementsPath := filepath.Join(userDir, filepath.Base(requirementsHandler.Filename))
 		requirementsFile, err := os.OpenFile(requirementsPath, os.O_WRONLY|os.O_CREATE, 0644)
 		if err != nil {
-			log.Fatalf("Error opening requirements file: %v\n", err)
+			log.Printf("Error opening requirements file: %v\n", err)
+			return
 		}
 		defer requirementsFile.Close()
 		n_requirements, err := io.Copy(requirementsFile, requirementsTempFile)
 		if err != nil {
-			log.Fatalf("Error copying requirements file to disk: %v\n", err)
+			log.Printf("Error copying requirements file to disk: %v\n", err)
+			return
 		}
 
 		trainTempFile, trainHandler, err := r.FormFile("train")
 		if err != nil {
-			log.Fatalf("Error reading train form file: %v\n", err)
+			log.Printf("Error reading train form file: %v\n", err)
+			return
 		}
 		defer trainTempFile.Close()
-		trainPath := userDir + filepath.Base(trainHandler.Filename)
+		trainPath := filepath.Join(userDir, filepath.Base(trainHandler.Filename))
 		trainFile, err := os.OpenFile(trainPath, os.O_WRONLY|os.O_CREATE, 0755)
 		if err != nil {
-			log.Fatalf("Error opening train file: %v\n", err)
+			log.Printf("Error opening train file: %v\n", err)
+			return
 		}
 		defer trainFile.Close()
 		n_train, err := io.Copy(trainFile, trainTempFile)
 		if err != nil {
-			log.Fatalf("Error copying train file to disk: %v\n", err)
+			log.Printf("Error copying train file to disk: %v\n", err)
+			return
 		}
 
 		dataTempFile, dataHandler, err := r.FormFile("data")
 		if err != nil {
-			log.Fatalf("Error reading data form file: %v\n", err)
+			log.Printf("Error reading data form file: %v\n", err)
+			return
 		}
 		defer dataTempFile.Close()
-		dataPath := userDir + filepath.Base(dataHandler.Filename)
+		dataPath := filepath.Join(userDir, filepath.Base(dataHandler.Filename))
 		dataFile, err := os.OpenFile(dataPath, os.O_WRONLY|os.O_CREATE, 0644)
 		if err != nil {
-			log.Fatalf("Error opening data file: %v\n", err)
+			log.Printf("Error opening data file: %v\n", err)
+			return
 		}
 		defer dataFile.Close()
 		defer os.Remove(dataPath)
 		n_data, err := io.Copy(dataFile, dataTempFile)
 		if err != nil {
-			log.Fatalf("Error copying data file to disk: %v\n", err)
+			log.Printf("Error copying data file to disk: %v\n", err)
+			return
 		}
 		err = archiver.TarGz.Open(dataPath, userDir)
 		if err != nil {
-			log.Fatalf("Error unzipping data dir: %v\n", err)
+			log.Printf("Error unzipping data dir: %v\n", err)
+			return
 		}
 
 		n := n_train + n_data + n_requirements
 		io.WriteString(w, fmt.Sprintf("%d bytes recieved and saved.\n", n))
 
-		venv := "venv-" + username
-		longCmdString := fmt.Sprintf(`source /usr/local/bin/virtualenvwrapper.sh; \\
-		mkvirtualenv -r %s %s; \\
-		python %s; \\
-		deactivate; \\
-		rmvirtualenv %s`,
-			requirementsPath, venv, trainPath, venv)
-		// log.Printf("Executing: \n%s\n", longCmdString)
-		// trainCmd := exec.Command("bash", "-c", longCmdString)
-		// trainOut, err := trainCmd.Output()
-		// if err != nil {
-		// 	log.Fatalf("Error executing %s: %v\n", longCmdString, err)
-		// 	io.WriteString(w, fmt.Sprintf("Failure executing %s\n", trainHandler.Filename))
-		// } else {
-		// 	log.Printf("Output: \n%s\n", string(trainOut))
-		// 	io.WriteString(w, string(trainOut))
-		// }
-
-		log.Printf("Launching docker...\n")
-
 		ctx := context.Background()
 		cli, err := docker.NewEnvClient()
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
 
-		// reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
-		reader, err := cli.ImagePull(ctx, "ubuntu:16.04", types.ImagePullOptions{})
-		// reader, err := cli.ImagePull(ctx, "floydhub/dl-docker", types.ImagePullOptions{})
+		// reader, err := cli.ImagePull(ctx, "wminshew/emrys", types.ImagePullOptions{})
+
+		// TODO: add project / job subfolders..
+		// TODO: won't hardlink if it already exists ... either need a work around or to not hard link
+		linkedDocker := filepath.Join(userDir, "Dockerfile")
+		err = os.Link("Dockerfile.user", linkedDocker)
+		defer os.Remove(linkedDocker)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
-		io.Copy(os.Stdout, reader)
+		buildCtxPath := filepath.Join(userDir + ".tar.gz")
+		log.Print(buildCtxPath)
+		ctxFiles, err := filepath.Glob(filepath.Join(userDir, "/*"))
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		err = archiver.TarGz.Make(buildCtxPath, ctxFiles)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		// defer os.Remove(buildCtxPath)
+		buildCtx, err := os.Open(buildCtxPath)
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		defer buildCtx.Close()
+		buildResp, err := cli.ImageBuild(ctx, buildCtx, types.ImageBuildOptions{
+			// TODO: add tags for emrys / project / job?
+			Tags: []string{username},
+			// TODO: explore Isolation: types.Isolation.IsHyperV
+			BuildArgs: map[string]*string{
+				"USER": &username,
+			},
+		})
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		defer buildResp.Body.Close()
+		io.Copy(os.Stdout, buildResp.Body)
 
 		resp, err := cli.ContainerCreate(ctx, &container.Config{
-			// Image: "dl-docker",
-			// Image: "alpine",
-			Image: "ubuntu:16.04",
-			// Cmd:   []string{"echo", "hello world"},
-			Cmd: []string{"/bin/bash", "-c", longCmdString},
-			Tty: true,
+			Image: username,
+			// Tty:   true,
 		}, nil, nil, "")
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
 
 		if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
 
-		// statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 		statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 		select {
 		case err := <-errCh:
 			if err != nil {
-				log.Fatal(err)
+				log.Print(err)
+				return
 			}
 		case <-statusCh:
 		}
 
 		out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
 
+		// TODO: Use TeeReader to log somewhere also?
 		_, err = io.Copy(os.Stdout, out)
 		if err != nil && err != io.EOF {
-			log.Fatal(err)
+			log.Print(err)
+			return
 		}
 
 	} else {
