8a0ef9650ee1af33c3f180d10ce6360d303cf6c2
wminshew <wminshew@gmail.com>
2018-04-25 18:10:32 -0700

    minor comment cleanup

--- db/Init.go
+++ db/Init.go
@@ -1,4 +1,3 @@
-// TODO: should this be moved to package main?
 package db
 
 import (
@@ -16,9 +15,6 @@ var (
 	dbName     = os.Getenv("DBNAME")
 )
 
-// TODO: db.Db is not a good name; think more here. Could just put in package main.
-// I guess eventually we won't export db -- just methods on db so
-// this will be fine.....
 var Db *sql.DB
 
 func Init() {
@@ -26,7 +22,6 @@ func Init() {
 
 	// postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbNetloc, dbPort, dbName)
-	// connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbName)
 	Db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
