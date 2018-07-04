48b2dd7fc25159ebb8a46af8d63b4edb857486c6
wminshew <wminshew@gmail.com>
2018-07-04 09:38:25 -0700

    minor commenting updates

--- db/Init.go
+++ db/Init.go
@@ -3,7 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	// blank import for psql driver
+	// psql driver
 	_ "github.com/lib/pq"
 	"github.com/wminshew/emrysserver/pkg/app"
 	"os"
@@ -25,7 +25,6 @@ func Init() {
 	app.Sugar.Infof("Initializing database...")
 
 	var err error
-	// postgresql://[user[:password]@][netloc][:port][,...][/dbname][?param1=value1&...]
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPassword, dbNetloc, dbPort, dbName)
 	if Db, err = sql.Open("postgres", connStr); err != nil {
 		app.Sugar.Errorf("Error connecting to database: %v", err)
