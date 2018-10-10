7aa13025e8c4df096def94cee1e398d3440090be
wminshew <wminshew@gmail.com>
2018-10-10 01:33:17 -0700

    added endpoint for miner updating image_downloaded status

---
+++ cmd/image/imageDownloaded.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/satori/go.uuid"
+	"github.com/wminshew/emrysserver/pkg/app"
+	"github.com/wminshew/emrysserver/pkg/db"
+	"github.com/wminshew/emrysserver/pkg/log"
+	"net/http"
+)
+
+// imageDownloaded handles updating image_built status in db by miners
+func imageDownloaded() app.Handler {
+	return func(w http.ResponseWriter, r *http.Request) *app.Error {
+		vars := mux.Vars(r)
+		jID := vars["jID"]
+		jUUID, err := uuid.FromString(jID)
+		if err != nil {
+			log.Sugar.Errorw("error parsing job ID",
+				"url", r.URL,
+				"err", err.Error(),
+			)
+			return &app.Error{Code: http.StatusBadRequest, Message: "error parsing job ID"}
+		}
+
+		return db.SetStatusImageDownloaded(r, jUUID)
+	}
+}
