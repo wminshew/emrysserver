a6e2ec8507a7aa6edacfc21266bfaa93b4460540
wminshew <wminshew@gmail.com>
2018-11-13 20:49:57 -0800

    added more devpi customization from yaml

--- cmd/devpi/docker-entrypoint.sh
+++ cmd/devpi/docker-entrypoint.sh
@@ -13,7 +13,7 @@ if [ "$1" = 'devpi' ]; then
     /usr/bin/devpi-server --init
   fi
 
-  exec /usr/bin/devpi-server --host 0.0.0.0 --port 3141
+  exec /usr/bin/devpi-server --replica-max-retries=${MAX_RETRIES-3} --host 0.0.0.0 --port 3141 ${DEVPI_DEBUG-"--debug"}
 fi
 
 exec "$@"
