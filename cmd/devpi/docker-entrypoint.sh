3a0a2ff8aee601608e1ce52e9ed0b249d2285c47
wminshew <wminshew@gmail.com>
2018-11-16 18:58:54 -0800

    updated devpi version

--- cmd/devpi/docker-entrypoint.sh
+++ cmd/devpi/docker-entrypoint.sh
@@ -2,18 +2,18 @@
 # inspiration: https://github.com/muccg/docker-devpi/blob/master/docker-entrypoint.sh
 
 function defaults {
-  : ${DEVPI_SERVERDIR="/devpi/server"}
-  export DEVPI_SERVERDIR
+  : ${DEVPISERVER_SERVERDIR="/devpi/server"}
+  export DEVPISERVER_SERVERDIR
 }
 
 defaults
 
 if [ "$1" = 'devpi' ]; then
-  if [ ! -f $DEVPI_SERVERDIR/.serverversion  ]; then
+  if [ ! -f $DEVPISERVER_SERVERDIR/.serverversion  ]; then
     /usr/bin/devpi-server --init
   fi
 
-  exec /usr/bin/devpi-server --replica-max-retries=${MAX_RETRIES-3} --host 0.0.0.0 --port 3141 ${DEVPI_DEBUG-"--debug"}
+  exec /usr/bin/devpi-server --replica-max-retries=${DEVPISERVER_MAX_RETRIES-3} --host 0.0.0.0 --port 3141 ${DEVPISERVER_DEBUG-"--debug"}
 fi
 
 exec "$@"
