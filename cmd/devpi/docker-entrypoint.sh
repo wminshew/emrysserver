882db91cab8f54268a8f671473b5049138c69763
wminshew <wminshew@gmail.com>
2018-12-06 18:20:37 -0800

    fixed bash bug with ENV-triggered --debug on devpi server

--- cmd/devpi/docker-entrypoint.sh
+++ cmd/devpi/docker-entrypoint.sh
@@ -13,7 +13,7 @@ if [ "$1" = 'devpi' ]; then
     /usr/bin/devpi-server --init
   fi
 
-  exec /usr/bin/devpi-server --replica-max-retries=${DEVPISERVER_MAX_RETRIES-3} --host 0.0.0.0 --port 3141 ${DEVPISERVER_DEBUG-"--debug"} 2>&1
+  exec /usr/bin/devpi-server --replica-max-retries=${DEVPISERVER_MAX_RETRIES-3} --host 0.0.0.0 --port 3141 ${DEVPISERVER_DEBUG:+"--debug"} 2>&1
 fi
 
 exec "$@"
