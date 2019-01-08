be2823c517c0456fd09091fe1ada03ffbc93dbd2
wminshew <wminshew@gmail.com>
2019-01-07 21:08:16 -0800

    added logging to sshd

--- cmd/notebook/entrypoint-sshd.sh
+++ cmd/notebook/entrypoint-sshd.sh
@@ -5,4 +5,8 @@ touch /root/.ssh/authorized_keys
 echo ${PUBLIC_KEY} > /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
-/usr/sbin/sshd -D -f ${SSHD_CONFIG}
+# rsyslogd
+# rc-service syslog start
+
+/usr/sbin/sshd -D -f ${SSHD_CONFIG} -e 2>&1
+# /usr/sbin/sshd -D -f ${SSHD_CONFIG}
