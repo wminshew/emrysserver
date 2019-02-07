c936c261463bdbda4c593bdbb463764fdd293d12
wminshew <wminshew@gmail.com>
2019-02-06 17:15:46 -0800

    added notebook flags to post & cancel job

--- cmd/user/postCancelJob.go
+++ cmd/user/postCancelJob.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"github.com/cenkalti/backoff"
 	"github.com/gorilla/mux"
 	"github.com/satori/go.uuid"
+	"github.com/wminshew/emrys/pkg/check"
 	"github.com/wminshew/emrysserver/pkg/app"
 	"github.com/wminshew/emrysserver/pkg/db"
 	"github.com/wminshew/emrysserver/pkg/log"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	del = "DELETE"
 )
 
 // postCancelJob handles user job cancellations
@@ -21,6 +31,60 @@ var postCancelJob app.Handler = func(w http.ResponseWriter, r *http.Request) *ap
 			"err", err.Error(),
 		)
 		return &app.Error{Code: http.StatusBadRequest, Message: "error parsing job ID"}
+	}
+
+	nbQuery := r.URL.Query().Get("notebook")
+	notebook := (nbQuery == "1")
+
+	if notebook {
+		ctx := r.Context()
+		client := http.Client{}
+		u := url.URL{
+			Scheme: "http",
+			Host:   "notebook-svc:8080",
+			Path:   "user",
+		}
+		q := u.Query()
+		q.Set("jID", jUUID.String())
+		u.RawQuery = q.Encode()
+
+		operation := func() error {
+			req, err := http.NewRequest(del, u.String(), nil)
+			if err != nil {
+				return err
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				return err
+			}
+			defer check.Err(resp.Body.Close)
+
+			if resp.StatusCode == http.StatusBadGateway {
+				return fmt.Errorf("server: temporary error")
+			} else if resp.StatusCode >= 300 {
+				b, _ := ioutil.ReadAll(resp.Body)
+				return backoff.Permanent(fmt.Errorf("server: %v", string(b)))
+			}
+
+			return nil
+		}
+		if err := backoff.RetryNotify(operation,
+			backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxBackoffRetries), ctx),
+			func(err error, t time.Duration) {
+				log.Sugar.Errorw("error deleting notebook user, retrying",
+					"method", r.Method,
+					"url", r.URL,
+					"err", err.Error(),
+				)
+			}); err != nil {
+			log.Sugar.Errorw("error deleting notebook user, aborting",
+				"method", r.Method,
+				"url", r.URL,
+				"err", err.Error(),
+			)
+			return &app.Error{Code: http.StatusInternalServerError, Message: "error canceling notebook job"}
+		}
 	}
 
 	return db.SetJobCanceledAndDebitUser(r, jUUID)
--- cmd/user/postJob.go
+++ cmd/user/postJob.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"github.com/cenkalti/backoff"
 	"github.com/gorilla/mux"
 	"github.com/satori/go.uuid"
+	"github.com/wminshew/emrys/pkg/check"
 	"github.com/wminshew/emrysserver/pkg/app"
 	"github.com/wminshew/emrysserver/pkg/db"
 	"github.com/wminshew/emrysserver/pkg/log"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"time"
+)
+
+const (
+	post              = "POST"
+	maxBackoffRetries = 5
 )
 
 // postJob handles new jobs posted by users
@@ -27,5 +38,64 @@ var postJob app.Handler = func(w http.ResponseWriter, r *http.Request) *app.Erro
 	jobID := uuid.NewV4()
 	w.Header().Set("X-Job-ID", jobID.String())
 
-	return db.InsertJob(r, uUUID, project, jobID)
+	nbQuery := r.URL.Query().Get("notebook")
+	notebook := (nbQuery == "1")
+
+	if notebook {
+		ctx := r.Context()
+		client := http.Client{}
+		u := url.URL{
+			Scheme: "http",
+			Host:   "notebook-svc:8080",
+			Path:   "user",
+		}
+		q := u.Query()
+		q.Set("jID", jobID.String())
+		u.RawQuery = q.Encode()
+
+		operation := func() error {
+			req, err := http.NewRequest(post, u.String(), nil)
+			if err != nil {
+				return err
+			}
+
+			resp, err := client.Do(req)
+			if err != nil {
+				return err
+			}
+			defer check.Err(resp.Body.Close)
+
+			if resp.StatusCode == http.StatusBadGateway {
+				return fmt.Errorf("server: temporary error")
+			} else if resp.StatusCode >= 300 {
+				b, _ := ioutil.ReadAll(resp.Body)
+				return backoff.Permanent(fmt.Errorf("server: %v", string(b)))
+			}
+
+			sshKeyBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return backoff.Permanent(err)
+			}
+			w.Header().Set("X-SSH-Key", string(sshKeyBytes))
+			return nil
+		}
+		if err := backoff.RetryNotify(operation,
+			backoff.WithContext(backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxBackoffRetries), ctx),
+			func(err error, t time.Duration) {
+				log.Sugar.Errorw("error adding notebook user, retrying",
+					"method", r.Method,
+					"url", r.URL,
+					"err", err.Error(),
+				)
+			}); err != nil {
+			log.Sugar.Errorw("error adding notebook user, aborting",
+				"method", r.Method,
+				"url", r.URL,
+				"err", err.Error(),
+			)
+			return &app.Error{Code: http.StatusInternalServerError, Message: "error posting notebook job"}
+		}
+	}
+
+	return db.InsertJob(r, uUUID, project, jobID, notebook)
 }
