a0175fa7f207680494f5e502d6a17c013980f595
wminshew <wminshew@gmail.com>
2019-03-13 16:14:09 -0700

    saves existing log when user cancels job

--- cmd/user/postCancelJob.go
+++ cmd/user/postCancelJob.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/cenkalti/backoff"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/satori/go.uuid"
 	"github.com/wminshew/emrys/pkg/check"
@@ -12,11 +13,13 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
 const (
-	del = "DELETE"
+	del        = "DELETE"
+	maxRetries = 5
 )
 
 // postCancelJob handles user job cancellations
@@ -32,13 +35,18 @@ var postCancelJob app.Handler = func(w http.ResponseWriter, r *http.Request) *ap
 		)
 		return &app.Error{Code: http.StatusBadRequest, Message: "error parsing job ID"}
 	}
+	log.Sugar.Infow("user canceled job",
+		"method", r.Method,
+		"url", r.URL,
+		"jID", jUUID,
+	)
 
 	nbQuery := r.URL.Query().Get("notebook")
 	notebook := (nbQuery == "1")
 
+	ctx := r.Context()
+	client := http.Client{}
 	if notebook {
-		ctx := r.Context()
-		client := http.Client{}
 		u := url.URL{
 			Scheme: "http",
 			Host:   "notebook-svc:8080",
@@ -85,6 +93,124 @@ var postCancelJob app.Handler = func(w http.ResponseWriter, r *http.Request) *ap
 			)
 			return &app.Error{Code: http.StatusInternalServerError, Message: "error canceling notebook job"}
 		}
+	}
+
+	// save log, if exists
+	mUUID, err := db.GetJobWinner(jUUID)
+	if err != nil {
+		log.Sugar.Errorw("error getting job winner",
+			"method", r.Method,
+			"url", r.URL,
+			"err", err.Error(),
+			"jID", jUUID,
+		)
+		return &app.Error{Code: http.StatusInternalServerError, Message: "error canceling notebook job"}
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"aud":   "emrys.io",
+		"exp":   time.Now().Add(time.Minute * 5).Unix(),
+		"iss":   "emrys.io",
+		"iat":   time.Now().Unix(),
+		"sub":   mUUID,
+		"scope": []string{"miner"},
+	})
+
+	authToken, err := token.SignedString([]byte(authSecret))
+	if err != nil {
+		log.Sugar.Errorw("error signing token",
+			"method", r.Method,
+			"url", r.URL,
+			"err", err.Error(),
+			"jID", jUUID,
+		)
+		return &app.Error{Code: http.StatusInternalServerError, Message: "error canceling notebook job"}
+	}
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   "job-svc",
+		Path:   fmt.Sprintf("job/%s/log", jUUID),
+	}
+	operation := func() error {
+		req, err := http.NewRequest(post, u.String(), strings.NewReader("JOB CANCELED."))
+		if err != nil {
+			return err
+		}
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", authToken))
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer check.Err(resp.Body.Close)
+
+		if resp.StatusCode == http.StatusBadGateway {
+			return fmt.Errorf("server: temporary error")
+		} else if resp.StatusCode >= 300 {
+			b, _ := ioutil.ReadAll(resp.Body)
+			return fmt.Errorf("server: %v", string(b))
+		}
+
+		return nil
+	}
+	if err := backoff.RetryNotify(operation,
+		backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries),
+		func(err error, t time.Duration) {
+			log.Sugar.Errorw("error posting cancellation to job output log--retrying",
+				"method", r.Method,
+				"url", r.URL,
+				"err", err.Error(),
+				"jID", jUUID,
+			)
+		}); err != nil {
+		log.Sugar.Errorw("error posting cancellation to job output log--abort",
+			"method", r.Method,
+			"url", r.URL,
+			"err", err.Error(),
+			"jID", jUUID,
+		)
+		return &app.Error{Code: http.StatusInternalServerError, Message: "error canceling notebook job"}
+	}
+	operation = func() error {
+		// POST with empty body signifies log upload complete
+		req, err := http.NewRequest(post, u.String(), nil)
+		if err != nil {
+			return err
+		}
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", authToken))
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer check.Err(resp.Body.Close)
+
+		if resp.StatusCode == http.StatusBadGateway {
+			return fmt.Errorf("server: temporary error")
+		} else if resp.StatusCode >= 300 {
+			b, _ := ioutil.ReadAll(resp.Body)
+			return fmt.Errorf("server: %v", string(b))
+		}
+
+		return nil
+	}
+	if err := backoff.RetryNotify(operation,
+		backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries),
+		func(err error, t time.Duration) {
+			log.Sugar.Errorw("error posting error to job output log--retrying",
+				"method", r.Method,
+				"url", r.URL,
+				"err", err.Error(),
+				"jID", jUUID,
+			)
+		}); err != nil {
+		log.Sugar.Errorw("error posting error to job output log--abort",
+			"method", r.Method,
+			"url", r.URL,
+			"err", err.Error(),
+			"jID", jUUID,
+		)
+		return &app.Error{Code: http.StatusInternalServerError, Message: "error canceling notebook job"}
 	}
 
 	return db.SetJobCanceledAndDebitUser(r, jUUID)
