b154f9a25c8ed691c6e52c258e5e7ffe21ab3643
wminshew <wminshew@gmail.com>
2019-03-30 02:04:08 -0700

    removed externalTrafficPolicy local from services; think it was actually causing a lot of issues; also added CORS back to image, registry

--- cmd/image/server.go
+++ cmd/image/server.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/gorilla/mux"
-	// "github.com/rs/cors"
+	"github.com/rs/cors"
 	"github.com/wminshew/emrys/pkg/validate"
 	"github.com/wminshew/emrysserver/pkg/app"
 	"github.com/wminshew/emrysserver/pkg/auth"
@@ -21,15 +21,16 @@ import (
 )
 
 var (
-	authSecret   = os.Getenv("AUTH_SECRET")
-	registryHost = os.Getenv("REGISTRY_HOST")
-	// debugCors        = (os.Getenv("DEBUG_CORS") == "true")
+	authSecret       = os.Getenv("AUTH_SECRET")
+	registryHost     = os.Getenv("REGISTRY_HOST")
+	debugCors        = (os.Getenv("DEBUG_CORS") == "true")
+	debugLog         = (os.Getenv("DEBUG_LOG") == "true")
 	devpiHost        = os.Getenv("DEVPI_HOST")
 	devpiTrustedHost string
 )
 
 func main() {
-	log.Init()
+	log.Init(debugLog)
 	defer func() {
 		if err := log.Sugar.Sync(); err != nil {
 			log.Sugar.Errorf("Error syncing log: %v\n", err)
@@ -79,22 +80,21 @@ func main() {
 	postBuildImagePath := fmt.Sprintf("/{jID:%s}", uuidRegexpMux)
 	rImageUser.Handle(postBuildImagePath, buildImage)
 
-	// c := cors.New(cors.Options{
-	// 	AllowedOrigins: []string{
-	// 		"https://www.emrys.io",
-	// 		"http://localhost:8080",
-	// 	},
-	// 	AllowedHeaders: []string{
-	// 		"*",
-	// 	},
-	// 	Debug: debugCors,
-	// })
-	// h := c.Handler(r)
-	//
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"https://www.emrys.io",
+			"http://localhost:8080",
+		},
+		AllowedHeaders: []string{
+			"*",
+		},
+		Debug: debugCors,
+	})
+	h := c.Handler(r)
+
 	server := http.Server{
-		Addr: ":8080",
-		// Handler:           log.Log(h),
-		Handler:           log.Log(r),
+		Addr:              ":8080",
+		Handler:           log.Log(h),
 		ReadHeaderTimeout: 15 * time.Second,
 		IdleTimeout:       620 * time.Second, // per https://cloud.google.com/load-balancing/docs/https/#timeouts_and_retries
 	}
--- cmd/registry/server.go
+++ cmd/registry/server.go
@@ -5,7 +5,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/gorilla/mux"
-	// "github.com/rs/cors"
+	"github.com/rs/cors"
 	"github.com/wminshew/emrys/pkg/validate"
 	"github.com/wminshew/emrysserver/pkg/app"
 	"github.com/wminshew/emrysserver/pkg/auth"
@@ -24,11 +24,12 @@ import (
 var (
 	authSecret   = os.Getenv("AUTH_SECRET")
 	registryHost = os.Getenv("REGISTRY_HOST")
-	// debugCors    = (os.Getenv("DEBUG_CORS") == "true")
+	debugCors    = (os.Getenv("DEBUG_CORS") == "true")
+	debugLog     = (os.Getenv("DEBUG_LOG") == "true")
 )
 
 func main() {
-	log.Init()
+	log.Init(debugLog)
 	defer func() {
 		if err := log.Sugar.Sync(); err != nil {
 			log.Sugar.Errorf("Error syncing log: %v\n", err)
@@ -75,22 +76,23 @@ func main() {
 	rJob.Use(checkImageDownloaded)
 	rJob.NewRoute().Handler(registryRP)
 
-	// c := cors.New(cors.Options{
-	// 	AllowedOrigins: []string{
-	// 		"https://www.emrys.io",
-	// 		"http://localhost:8080",
-	// 	},
-	// 	AllowedHeaders: []string{
-	// 		"Origin", "Accept", "Content-Type", "X-Requested-With", "Authorization",
-	// 	},
-	// 	Debug: debugCors,
-	// })
-	// h := c.Handler(r)
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"https://www.emrys.io",
+			"http://localhost:8080",
+		},
+		AllowedHeaders: []string{
+			"*",
+			// "Origin", "Accept", "Content-Type", "X-Requested-With", "Authorization",
+		},
+		Debug: debugCors,
+	})
+	h := c.Handler(r)
 
 	server := http.Server{
-		Addr:    ":5000",
-		Handler: log.Log(r),
-		// Handler:           log.Log(h),
+		Addr: ":5000",
+		// Handler: log.Log(r),
+		Handler:           log.Log(h),
 		ReadHeaderTimeout: 15 * time.Second,
 		IdleTimeout:       620 * time.Second, // per https://cloud.google.com/load-balancing/docs/https/#timeouts_and_retries
 	}
