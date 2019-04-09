35b6d98a3f185118f4d3d5894a687f44cf7854db
wminshew <wminshew@gmail.com>
2019-04-09 01:40:42 -0700

    added extra catch for empty stripe account IDs

--- cmd/miner/connect.go
+++ cmd/miner/connect.go
@@ -32,17 +32,25 @@ var connect app.Handler = func(w http.ResponseWriter, r *http.Request) *app.Erro
 			"err", err.Error(),
 		)
 		return &app.Error{Code: http.StatusInternalServerError, Message: "internal error"}
+	} else if acctID == "" {
+		log.Sugar.Infow("no stripe payout account",
+			"method", r.Method,
+			"url", r.URL,
+			"mID", mUUID,
+		)
+		return &app.Error{Code: http.StatusBadRequest, Message: "no stripe payout account. " +
+			"Please verify your payout information at https://www.emrys.io/account and reach out to support if problems continue."}
 	}
 
 	_, err = account.GetByID(acctID, nil)
 	if err != nil {
-		log.Sugar.Errorw("miner connected w/o stripe account",
+		log.Sugar.Errorw("miner's stripe account not recognized or inactive",
 			"method", r.Method,
 			"url", r.URL,
 			"err", err.Error(),
 		)
 		return &app.Error{Code: http.StatusBadRequest, Message: "your stripe account is inactive or non-existent. " +
-			"Please verify your payout information on https://www.emrys.io and reach out to support if problems continue."}
+			"Please verify your payout information on https://www.emrys.io/account and reach out to support if problems continue."}
 	}
 
 	q := r.URL.Query()
