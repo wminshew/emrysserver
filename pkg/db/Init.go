4a3f928b57643d60a7666b76cb1ca36d341e8777
wminshew <wminshew@gmail.com>
2019-04-17 23:38:11 -0700

    added sensible db connection defaults

--- pkg/db/Init.go
+++ pkg/db/Init.go
@@ -7,14 +7,22 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/wminshew/emrysserver/pkg/log"
 	"os"
+	"strconv"
+	"time"
 )
 
 var (
-	dbUser     = os.Getenv("DBUSER")
-	dbPassword = os.Getenv("DBPASSWORD")
-	dbNetloc   = os.Getenv("DBNETLOC")
-	dbPort     = os.Getenv("DBPORT")
-	dbName     = os.Getenv("DBNAME")
+	dbUser               = os.Getenv("DBUSER")
+	dbPassword           = os.Getenv("DBPASSWORD")
+	dbNetloc             = os.Getenv("DBNETLOC")
+	dbPort               = os.Getenv("DBPORT")
+	dbName               = os.Getenv("DBNAME")
+	dbMaxOpenConnsStr    = os.Getenv("DBMAXOPENCONNS")
+	dbMaxOpenConns       = 10
+	dbMaxIdleConnsStr    = os.Getenv("DBMAXIDLECONNS")
+	dbMaxIdleConns       = 5
+	dbMaxConnLifetimeStr = os.Getenv("DBCONNMAXLIFETIME")
+	dbMaxConnLifetime    = 10 * time.Minute
 )
 
 var db *sql.DB
@@ -34,6 +42,35 @@ func Init() {
 		log.Sugar.Errorf("Error pinging database: %v", err)
 		panic(err)
 	}
+
+	if dbMaxOpenConnsStr != "" {
+		dbMaxOpenConns, err = strconv.Atoi(dbMaxOpenConnsStr)
+		if err != nil {
+			log.Sugar.Errorf("Error converting max_open_conns: %v", err)
+			panic(err)
+		}
+	}
+	db.SetMaxOpenConns(dbMaxOpenConns)
+
+	if dbMaxIdleConnsStr != "" {
+		dbMaxIdleConns, err = strconv.Atoi(dbMaxIdleConnsStr)
+		if err != nil {
+			log.Sugar.Errorf("Error converting max_idle_conns: %v", err)
+			panic(err)
+		}
+	}
+	db.SetMaxIdleConns(dbMaxIdleConns)
+
+	if dbMaxConnLifetimeStr != "" {
+		dbMaxConnLifetimeMin, err := strconv.Atoi(dbMaxConnLifetimeStr)
+		if err != nil {
+			log.Sugar.Errorf("Error converting max_conn_lifetime: %v", err)
+			panic(err)
+		}
+
+		dbMaxConnLifetime = time.Duration(dbMaxConnLifetimeMin) * time.Minute
+	}
+	db.SetConnMaxLifetime(dbMaxConnLifetime)
 }
 
 // Close closes the database connection
