bae1bc581e7947c8f89040fe3e7fd5a146ffa25c
wminshew <wminshew@gmail.com>
2019-08-14 13:42:18 -0700

    fixed binary operator error in entrypoint.sh

--- cmd/image/job-dockerfiles/entrypoint.sh
+++ cmd/image/job-dockerfiles/entrypoint.sh
@@ -5,7 +5,7 @@ CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 
 if [ -f "$CONDA_ENV" ]; then
-  CONDA_ENV_NAME=`sed -n 's/name: //pI' "$CONDA_ENV"`
+  CONDA_ENV_NAME=$(sed -n 's/name: //pI' "$CONDA_ENV")
   conda env create -q -f "$CONDA_ENV"
 else
   CONDA_ENV_NAME=user
@@ -14,14 +14,14 @@ fi
 conda activate $CONDA_ENV_NAME
 
 if [ -f "$PIP_REQS" ]; then
-  if ! [ `conda list "^pip" | grep "^pip"` ]; then
+  if [[ -z $(conda list "^pip" | grep "^pip") ]]; then
     conda install -q -y -n $CONDA_ENV_NAME pip 1>/dev/null
   fi
   pip --no-cache-dir --timeout=30 --retries=10 install --progress-bar off -r "$PIP_REQS"
 fi
 
 if [ "$NOTEBOOK" = 'true' ]; then
-  if ! [ `command -v jupyter` ]; then
+  if [[ -z $(command -v jupyter) ]]; then
     conda install -q -y -n $CONDA_ENV_NAME jupyter 1>/dev/null
   fi
 
